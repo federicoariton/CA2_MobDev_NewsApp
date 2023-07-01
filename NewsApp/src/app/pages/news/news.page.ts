6b0d6a7f5b76129e15237cdbc9f2675ee28cceb1
federicoariton <federicoariton@outlook.es>
2023-07-01 23:54:11 +0100

    Fix the app for deploy

--- NewsApp/src/app/pages/news/news.page.ts
+++ NewsApp/src/app/pages/news/news.page.ts
@@ -3,12 +3,12 @@ import { HttpClient } from "@angular/common/http";
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
-// const API_URL = environment.API_URL;
-// const API_KEY = environment.API_KEY;
-
+// Define the API key and URL from the environment
 const api1Key = environment.api1.API_KEY;
 const api1Url = environment.api1.API_URL;
 
+
+// Define the interface for a news article
 interface NewsArticle {
   source: {
     id: string;
@@ -31,13 +31,17 @@ interface NewsArticle {
 export class NewsPage implements OnInit {
   articles: NewsArticle[] = [];
 
+
+  // Load the articles when the component is constructed
   constructor(public httpClient: HttpClient, private router: Router) {
     this.loadArticles();
   }
 
   loadArticles() {
+     // Make an HTTP GET request to retrieve the top headlines
     this.httpClient.get<any>(`${api1Url}/top-headlines?country=us&category=general&apiKey=${api1Key}`).subscribe(
       (response: any) => {
+         // Store the retrieved articles in the component's 'articles' property
         console.log(response);
         this.articles = response.articles;
       },
@@ -47,6 +51,7 @@ export class NewsPage implements OnInit {
     );
   }
 
+   // Open the article URL in a new browser tab/window
   openArticle(article: NewsArticle) {
     window.open(article.url, '_blank');
   }
